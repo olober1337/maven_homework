e27942abbc916a4d29ea2c2954649aaef2595c22
mcarmses15 <vadim.sukhetskii@gmail.com>
2021-09-03 10:41:42 +0300

    Homework 18 - redone the threadMethod with creating 10000 threads.

--- src/main/java/homework18/first/AtomicAndSecurityTest.java
+++ src/main/java/homework18/first/AtomicAndSecurityTest.java
@@ -4,28 +4,26 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicAndSecurityTest {
 
-    private static final AtomicInteger atomicInteger = new AtomicInteger(10);
+    private static final AtomicInteger atomicInteger = new AtomicInteger(0);
 
 
     public static void main(String[] args) throws InterruptedException {
 
         Thread thread1 = new Thread(AtomicAndSecurityTest::threadMethod);
-        Thread thread2 = new Thread(AtomicAndSecurityTest::threadMethod);
-        thread1.setName("Thread 1 is running ");
-        thread2.setName("Thread 2 is running ");
         thread1.start();
+        Thread thread2 = new Thread(AtomicAndSecurityTest::threadMethod);
         thread2.start();
 
-        Thread.sleep(2000);
-        System.out.println("AtomicInteger" + atomicInteger.get());
+        Thread.sleep(10000);
+        System.out.println("AtomicInteger " + atomicInteger.get());
 
     }
 
     private static synchronized void threadMethod() {
 
         for (int i = 10000; i > 0; i--) {
-            System.out.println(Thread.currentThread().getName() + i);
+            Thread thread = new Thread(atomicInteger::decrementAndGet);
+            thread.start();
         }
-        atomicInteger.decrementAndGet();
     }
 }
